0644d226453e76754f3ad0eb06fb25b7e92fbc55
Dylan Schoop <42898108+CurleyDS@users.noreply.github.com>
2022-05-10 14:14:44 +0200

    added fish, bubbles and water

--- src/game.ts
+++ src/game.ts
@@ -9,6 +9,9 @@ import waterImage from "./images/water.jpg"
 const pixi = new PIXI.Application({ width: 800, height: 450 })
 document.body.appendChild(pixi.view)
 
+let water: PIXI.Sprite;
+let fish: PIXI.Sprite;
+
 //
 // STAP 2 - preload alle afbeeldingen
 //
@@ -22,6 +25,16 @@ loader.load(()=>loadCompleted())
 // STAP 3 - maak een sprite als de afbeeldingen zijn geladen
 //
 function loadCompleted() {
-    let fish = new PIXI.Sprite(loader.resources["fishTexture"].texture!)
+    water = new PIXI.Sprite(loader.resources["waterTexture"].texture!)
+    pixi.stage.addChild(water)
+    
+    fish = new PIXI.Sprite(loader.resources["fishTexture"].texture!)
+    fish.anchor.set(0)
     pixi.stage.addChild(fish)
+    pixi.ticker.add((delta)=>update(delta));
+}
+
+function update(delta: number) {
+    fish.x += delta * 1;
+    fish.rotation += delta * 0.1;
 }
